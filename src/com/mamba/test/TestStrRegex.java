1042c691072ac3847cfa23391d9c7d8223886295
Danny <Danny>
2016-05-06 20:34:25 +0800

    add

--- src/com/mamba/test/TestStrRegex.java
+++ src/com/mamba/test/TestStrRegex.java
@@ -3,9 +3,7 @@ package com.mamba.test;
 import java.util.regex.Pattern;
 
 public class TestStrRegex {
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
 		String text = "This is the text to be searched for occurrence of the Jen pattern.";
 		String pat = ".*Jen.*";
